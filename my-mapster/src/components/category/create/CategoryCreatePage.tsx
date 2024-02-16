f4b4dcfd75b6e9e9666eb30e76e8656b464ee155
novakvova <novakvova@gmail.com>
2024-02-16 17:49:19 +0200

    Create category frontend

--- my-mapster/src/components/category/create/CategoryCreatePage.tsx
+++ my-mapster/src/components/category/create/CategoryCreatePage.tsx
@@ -1,9 +1,103 @@
+import {useNavigate} from "react-router-dom";
+import {ICategoryCreate, IUploadedFile} from "./types.ts";
+import {Button, Form, Input, Row, Upload} from "antd";
+import TextArea from "antd/es/input/TextArea";
+import { PlusOutlined } from '@ant-design/icons';
+import {UploadChangeParam} from "antd/es/upload";
+import http_common from "../../../http_common.ts";
 
 const CategoryCreatePage = () => {
+    const navigate = useNavigate();
+
+    const [form] = Form.useForm<ICategoryCreate>();
+
+    const onSubmit = async (values: ICategoryCreate) => {
+        try {
+            await http_common.post("/api/categories", values, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+            });
+            navigate('/');
+        }
+        catch(ex) {
+            console.log("Exception create category", ex);
+        }
+    }
+
     return (
         <>
             <h1>Додати категорію</h1>
-            
+            <Row gutter={16}>
+                <Form form={form}
+                      onFinish={onSubmit}
+                      layout={"vertical"}
+                      style={{
+                          minWidth: '100%',
+                          display: 'flex',
+                          flexDirection: 'column',
+                          justifyContent: 'center',
+                          padding: 20,
+                      }}
+                      >
+                    <Form.Item
+                        label="Назва"
+                        name="name"
+                        htmlFor="name"
+                        rules={[
+                            {required: true, message: 'Це поле є обов\'язковим!'},
+                            {min: 3, message: 'Назва повинна містити мінімум 3 символи!'},
+                        ]}
+                    >
+                        <Input autoComplete="name"/>
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Опис"
+                        name="description"
+                        htmlFor="description"
+                        rules={[
+                            {required: true, message: 'Це поле є обов\'язковим!'},
+                            {min: 10, message: 'Опис повинен містити мінімум 10 символів!'},
+                        ]}
+                    >
+                        <TextArea/>
+                    </Form.Item>
+                    <Form.Item
+                        name="file"
+                        label="Фото"
+                        valuePropName="file"
+                        getValueFromEvent={(e: UploadChangeParam) => {
+                            const image = e?.fileList[0] as IUploadedFile;
+                            return image?.originFileObj;
+                        }}
+                        rules={[{required: true, message: 'Оберіть фото категорії!'}]}
+                    >
+                        <Upload
+                            showUploadList={{showPreviewIcon: false}}
+                            beforeUpload={() => false}
+                            accept="image/*"
+                            listType="picture-card"
+                            maxCount={1}
+                        >
+                            <div>
+                                <PlusOutlined/>
+                                <div style={{marginTop: 8}}>Upload</div>
+                            </div>
+                        </Upload>
+                    </Form.Item>
+                    <Row style={{display: 'flex', justifyContent: 'center'}}>
+                        <Button style={{margin: 10}} type="primary" htmlType="submit">
+                            Додати
+                        </Button>
+                        <Button style={{margin: 10}} htmlType="button" onClick={() =>{ navigate('/')}}>
+                            Скасувати
+                        </Button>
+                    </Row>
+                </Form>
+            </Row>
+
+
         </>
     )
 }
--- my-mapster/src/components/category/create/types.ts
+++ my-mapster/src/components/category/create/types.ts
@@ -1,5 +1,17 @@
 export interface ICategoryCreate {
     name: string;
-    file: File;
+    file: File|undefined;
     description: string;
+}
+
+export interface IUploadedFile {
+    lastModified: number;
+    lastModifiedDate: Date;
+    name: string;
+    originFileObj: File;
+    percent: number;
+    size: number;
+    thumbUrl: string;
+    type: string;
+    uid: string;
 }
