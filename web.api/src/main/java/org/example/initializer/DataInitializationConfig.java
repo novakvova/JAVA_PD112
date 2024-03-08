3d21642c0b14290821c2aeb7f8b68549da52bede
novakvova <novakvova@gmail.com>
2024-03-08 16:19:25 +0200

    Is good jwt auth

--- web.api/src/main/java/org/example/initializer/DataInitializationConfig.java
+++ web.api/src/main/java/org/example/initializer/DataInitializationConfig.java
@@ -2,20 +2,25 @@ package org.example.initializer;
 
 import com.github.javafaker.Faker;
 import lombok.AllArgsConstructor;
+import org.example.constants.Roles;
 import org.example.entities.CategoryEntity;
+import org.example.entities.RoleEntity;
+import org.example.entities.UserEntity;
+import org.example.entities.UserRoleEntity;
 import org.example.repositories.CategoryRepository;
+import org.example.repositories.RoleRepository;
+import org.example.repositories.UserRepository;
+import org.example.repositories.UserRoleRepository;
 import org.example.storage.FileSaveFormat;
 import org.example.storage.StorageService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -26,6 +31,11 @@ public class DataInitializationConfig {
     private final CategoryRepository categoryRepository;
     private final StorageService storageService;
 
+    private final RoleRepository roleRepository;
+    private final UserRepository userRepository;
+    private final UserRoleRepository userRoleRepository;
+    private final PasswordEncoder passwordEncoder;
+
     @Bean
     @Transactional
     public CommandLineRunner initData() {
@@ -33,6 +43,8 @@ public class DataInitializationConfig {
             if (categoryRepository.count() < 100) {
                 addSampleCategories();
             }
+            seedRole();
+            seedUser();
         };
     }
 
@@ -44,6 +56,42 @@ public class DataInitializationConfig {
                 .collect(Collectors.toList());
 
         categoryRepository.saveAll(categories);
+    }
+
+    private void seedRole() {
+        if(roleRepository.count() == 0) {
+            RoleEntity admin = RoleEntity
+                    .builder()
+                    .name(Roles.Admin)
+                    .build();
+            roleRepository.save(admin);
+            RoleEntity user = RoleEntity
+                    .builder()
+                    .name(Roles.User)
+                    .build();
+            roleRepository.save(user);
+        }
+    }
+
+    private void seedUser() {
+        if(userRepository.count() == 0) {
+            var user = UserEntity
+                    .builder()
+                    .email("admin@gmail.com")
+                    .firstName("Микола")
+                    .lastName("Підкаблучник")
+                    .phone("+380 97 67 56 464")
+                    .password(passwordEncoder.encode("123456"))
+                    .build();
+            userRepository.save(user);
+            var role = roleRepository.findByName(Roles.Admin);
+            var ur = UserRoleEntity
+                    .builder()
+                    .role(role)
+                    .user(user)
+                    .build();
+            userRoleRepository.save(ur);
+        }
     }
 
     private CategoryEntity createRandomCategory(Faker faker) {
